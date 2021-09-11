1c74d20759be4198fb889583224861d3f67a2899
Ubuntu <ubuntu@ip-172-31-74-112.ec2.internal>
2021-09-11 02:12:24 +0000

    rectangle math program

--- rectanglemath.py
+++ rectanglemath.py
@@ -1,3 +1,19 @@
-"""
-Your module description
-"""
+#!/usr/bin/env python3
+
+# Created by: Rodas Nega
+# Created on: Sept 2021
+# This is a program that calculates the area and perimeter of a rectangle with dimensions 5cm x 3cm
+
+
+def main():
+    # a function that calculates the area and perimeter of the rectangle
+
+    print("Rectangle with dimensions 5 cm and 3 cm: ")
+    print("5 cm x 3 cm")
+    print("")
+    print("Area is {} cm².".format(3 * 5))
+    print("Perimeter is {} cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
